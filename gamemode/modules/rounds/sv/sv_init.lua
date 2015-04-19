926958629d35b66589b44f42c4ff298ebafef6ad
Snappey <jonathan.nut@gmail.com>
2015-04-19 13:46:32 +0100

    Round system fixed

--- gamemode/modules/rounds/sv/sv_init.lua
+++ gamemode/modules/rounds/sv/sv_init.lua
@@ -4,12 +4,14 @@ local round = CM.Modules["Rounds"]
 function round.StartRound()
 	if round.ongoing then MsgC(Color(255,0,0) , "ERROR: ", Color(250,250,250), "Round already started, stop the current one before starting another!") return end
 
+	print(round.timer, round.tick, round.tickTotal, round.length)
 	timer.Create(round.timer, round.tick, round.tickTotal, round.Tick)
-	timer.Create("RoundFinished", round.length, 1, round.Finished)
+	timer.Create("RoundFinished", round.length, 0, round.Finished)
 end
 
 function round.Tick()
 	hook.Call("RoundTick")
+	MsgC("INFO: ",Color(0,255,0), " Round tick completed! \n")
 end
 
 function round.Finished()
@@ -19,8 +21,18 @@ end
 
 function round.Stop()
 	if timer.Exists(round.timer) || timer.Exists("RoundFinished") then
-		timer.Destroy(round.timer)
-		timer.Destroy("RoundFinished")
+		timer.Remove(round.timer)
+		timer.Remove("RoundFinished")
 	end	
-	MsgC("INFO: ",Color(0,255,0), " Timers destroyed, round finished!")
+	MsgC("INFO: ",Color(0,255,0), " Timers destroyed, round finished! \n")
 end
+
+function round.GetTotalTimeLeft()
+	return timer.TimeLeft("RoundFinished")
+end
+
+function round.GetTickTimeLeft()
+	return timer.TimeLeft(round.timer)
+end
+
+CM.Modules["Rounds"] = round
