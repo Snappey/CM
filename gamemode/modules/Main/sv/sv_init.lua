3a2726844a345ded34ec462506b5829fd3e1daba
Snappey <jonathan.nut@gmail.com>
2015-04-20 19:47:41 +0100

    Cleaned up Main module

--- gamemode/modules/Main/sv/sv_init.lua
+++ gamemode/modules/Main/sv/sv_init.lua
@@ -8,24 +8,18 @@ function Main.Init()
 end
 
 function Main.SetupFactions()
-	Main.Factions["City Rebels"] = faction.Create("City Rebels")
-	Main.Factions["Mafia"] = faction.Create("Mafia")
-	Main.Factions["Government"] = faction.Create("Government")
+	Main.CreateFactions()
 
 	Main.FactionLoadout()
 end
 
-function Main.FactionLoadout()
-	Main.Factions["City Rebels"]:SetLoadout( {"weapon_crowbar"} )
-
-	Main.Factions["Mafia"]:SetLoadout( {"weapon_crowbar"} )
-
-	Main.Factions["Government"]:SetLoadout( {"weapon_crowbar"} )
-	print("OLO")
-end
 
 hook.Add("PostInit", "KickStartGamemode", function() -- Start the main module once core has finished loading
 	Main.Init()
+end)
+
+concommand.Add("ForceLoadout", function(ply,cmd,args)
+	Main.Loadout(ply)
 end)
 
 CM.Modules["Main"] = Main
