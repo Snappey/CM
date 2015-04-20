e95df01e26ddffc050f87d8ff45fbc9c8700f449
Snappey <jonathan.nut@gmail.com>
2015-04-20 15:10:51 +0100

    Base Factions Module Setup

--- gamemode/modules/Factions/sv/sv_faction.lua
+++ gamemode/modules/Factions/sv/sv_faction.lua
@@ -7,14 +7,19 @@ function faction.Create(name)
 	setmetatable(fctn, faction)
 	fctn.name = name
 	fctn.loadout = {}
+	fctn.colour = Color(0,0,0)
 	faction.id = faction.id + 1; fctn.id = faction.id
 	return fctn
 end
 
 function faction.SetLoadout(tbl)
+	self.loadout = tbl
+end
 
+function faction.AddWeapon(wep)
+	table.insert(self.loadout, 1, wep)
 end
 
 function faction.SetColour(r,g,b)
-
+	self.colour = Color(r,g,b)
 end
